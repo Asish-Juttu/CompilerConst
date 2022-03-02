cba2f3eda0998562e132e5727e728032312249eb
Rahulbiju315 <rahulpikslit@gmail.com>
2022-03-02 20:47:28 +0530

    pl

--- toString.c
+++ toString.c
@@ -99,7 +99,6 @@ char* nonTermToStr(NonTerminal nt){
         case CONDITIONAL_STMT : return "<CONDITIONAL_STMT>";
         case IO_STMT : return "<IO_STMT>";
         case ARITHMETIC_EXPRESSION : return "<ARITHMETIC_EXPRESSION>";
-        case OPERATOR : return "<OPERATOR>";
         case BOOLEAN_EXPRESSION : return "<BOOLEAN_EXPRESSION>";
         case VAR : return "<VAR>";
         case LOGICAL_OP : return "<LOGICAL_OP>";
