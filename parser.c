084967c62501e33aeb71447384302d65584ea9e0
Prane0204 <85984381+Prane0204@users.noreply.github.com>
2022-03-02 20:10:27 +0530

    Updated

--- parser.c
+++ parser.c
@@ -295,6 +295,14 @@ void initGrammar(Grammar* grammar){
     initRuleArray(grammar, ITERATIVE_STMT,1);
     addRule(grammar, ITERATIVE_STMT, iterativeStmt0, 7, 0);
 
+    Symbol definetypestmt0[] = {{1,TK_DEFINETYPE}, {0,A}, {1,TK_RUID}, {1,TK_AS}, {1,TK_RUID}};
+    initRuleArray(grammar, DEFINE_TYPE_STATEMENT,1);
+    addRule(grammar, DEFINE_TYPE_STATEMENT, definetypestmt0, 5, 0);
+ 
+    Symbol A0[] = {{1,TK_RECORD},{1,TK_UNION}};
+    initRuleArray(grammar, A,1);
+    addRule(grammar, A, A0, 2, 0);
+
     // initialize nset at the end
     // initLocations(grammar);
 }
