b94958454a00d95c1f86be1a595a9b3c7ffd7b8c
Rahulbiju315 <rahulpikslit@gmail.com>
2022-04-15 01:52:03 +0530

    dfghfgh

--- astPrint.c
+++ astPrint.c
@@ -4,21 +4,44 @@ void printTabs(int n){
     for(int i = 0; i < n; i++) printf("\t");
 }
 
-void print(void str, int n){
+void print(char* str, int n){
     printTabs(n);
-    printf("%s", str);
+    printf("%s ", str);
+}
+
+void printFloat(float f, int n){
+    printTabs(n);
+    printf("%f", f);
+}
+
+void printInt(int i, int n){
+    printTabs(n);
+    printf("%d", i);
 }
 
 void printDataType(Datatype dt, int n){
+    char* str;
 
+    if(dt == DT_PRIMITIVE){
+        str = "DATATYPE: PRIMITIVE";
+    }
+    else if(dt == DT_UNION){
+        str = "DATATYPE: UNION";
+    }
+    else if(dt == DT_RECORD){
+        str = "DATATYPE: RECORD";
+    }
+
+    print(str, n);
 }
+
 void printTypeDefinition(Ast_TypeDefinition* t, int n){
     print("Ast_TypeDefinition", n);
     n = n + 1;
     print(t->id1, n);
-    printf("\n");
+    printf("");
     print(t->id2, n);
-    printf("\n");
+    printf("");
     printDataType(t->datatype, n);
     printFieldDefinitions(t->fieldDefinitions, n);
 }
@@ -31,9 +54,6 @@ void printOtherStmts(Ast_OtherStmts* t, int n){
     for(int i = 0; i < list->size; i++){
         printStmts(nodeToAst(list->nodes[i], stmts), n);
     }
-
-
-
 }
 
 void printDeclarations(Ast_Declarations* t, int n){
@@ -97,7 +117,7 @@ void printSingleOrRecId(Ast_SingleOrRecId* t,int n){
     print("Ast_SingleOrRecId",n);
     n = n + 1;
     print(t->id, n);
-    printf("\n");
+    printf("");
      AstList* list = t->fieldNameList;
     n = n + 1;
     for(int i = 0; i < list->size; i++){
@@ -115,7 +135,7 @@ void printOptionSingleConstructed(Ast_OptionSingleConstructed* t, int n){
 void printDatatype(Ast_Datatype* t,int n){
      print("Ast_Datatype",n);
      print(t->name,n);
-     printf("\n");
+     printf("");
 
 }
 
@@ -123,7 +143,7 @@ void printFieldDefinition(Ast_FieldDefinition* t, int n){
     print("Ast_FieldDefinition",n);
     printDataType(t->fieldType, n);
     print(t->id,n);
-    printf("\n");
+    printf("");
 
 }
 
@@ -131,9 +151,9 @@ void printDefineTypeStmt(Ast_DefineTypeStmt* t, int n){
     print("Ast_FieldDefinition",n);
     printDataType(t->datatype, n);
     print(t->idOld,n);
-    printf("\n");
+    printf("");
     print(t->idNew,n);
-    printf("\n");
+    printf("");
 
 
     
@@ -150,16 +170,16 @@ void printA(Ast_A* t, int n){
 void printNum(Ast_Num* t, int n){
     print("Ast_Num",n);
     print(t->val,n);
-    printf("\n");
+    printf("");
     
 
 }
 
 void printRnum(Ast_Rnum* t, int n){
     print("Ast_Rnum",n);
-    print(t->val,n);
-    printf("\n");
-    
+    n = n + 1;
+    printFloat(t->val,n);
+    printf("");
 
 }
 
@@ -167,9 +187,9 @@ void printDeclaration(Ast_Declaration* t, int n){
     print("Ast_Declaration",n);
     printDataType(t->type, n);
     print(t->id,n);
-    printf("\n");
+    printf("");
     print(t->isGlobal,n);
-    printf("\n");
+    printf("");
 
 
 
@@ -253,27 +273,60 @@ void printArithmeticExpression(Ast_ArithmeticExpression* t, int n){
 
 }
 
+void printRelop(RelationalOperator op, int n){
+    if(op == ROP_Eq){
+        print("==", n);
+    }
+    else if(op == ROP_Ge){
+        print(">=", n);
+    }
+    else if(op == ROP_Gt){
+        print(">", n);
+    }
+    else if(op == ROP_Le){
+        print("<=", n);
+    }
+    else if(op == ROP_Lt){
+        print("<", n);
+    }
+    else if(op == ROP_Ne){
+        print("!=", n);
+    }
+}
 void printBooleanExpression(Ast_BooleanExpression* t, int n){
     print("Ast_ArithmeticExpression", n);
-    printArithmeticExpression(t->left,n);
-    printArithmeticExpression(t->right,n);
+    n = n + 1;
 
+    if(t->bexpType == BEXP_BOOL_OP){
+        printBooleanExpression(boolOpBexp(t)->left, n);
+        printLop(boolOpBexp(t)->op, n);
+        printBooleanExpression(boolOpBexp(t)->right, n);
 
+    }
+    else if(t->bexpType == BEXP_VAR_COMP){
+        printVar(varCompBexp(t)->left, n);
+        printRelop(varCompBexp(t)->op, 0);
+        printVar(varCompBexp(t)->right, 0);
+        printf("\n");
+    }
 }
 
 void printAssignmentStmt(Ast_AssignmentStmt* t, int n){
-    print("Ast_AssignmentStmt", n);
+    print("Ast_AssignmentStmt\n", n);
+    n = n + 1;
     printSingleOrRecId(t->singleOrRecId,n);
-    printArithmeticExpression(t->expression,n);
+    printf(" = ");
+    printArithmeticExpression(t->expression, 0);
 
 }
 
 void printFunCallStmt(Ast_FunCallStmt* t, int n){
     print("Ast_FunCallStmt", n);
-    printIdList(t->outputIdList,n);
-    printIdList(t->inputIdList,n);
-    print(t->funId,n);
-    printf("\n");
+    n = n + 1;
+    print(t->funId, n);
+    printIdList(t->outputIdList, 0);
+    printf("<-");
+    printIdList(t->inputIdList, 0);
 
 }
 
@@ -311,10 +364,21 @@ void elsePart(Ast_ElsePart* t){
 
 }
 
-void logicalOperator(Ast_LogicalOperator* t, int n){
+void printLop(LogicalOperator lop, int n){
+    if(lop == LOP_AND){
+        print("AND", n);
+    }
+    else if(lop == LOP_OR){
+        print("OR", n);
+    }
+    else if(lop == LOP_NOT){
+        print("NOT", n);
+    }
+}
+void printLogicalOperator(Ast_LogicalOperator* t, int n){
     print("Ast_LogicalOperator", n);
-    printLogicalOperator(t->op,n);
-
+    n = n + 1;
+    printLop(t->op,n);
 }
 
 void relationalOperator(Ast_RelationalOperator* t, int n){
@@ -323,14 +387,28 @@ void relationalOperator(Ast_RelationalOperator* t, int n){
 
 }
 
+void printAop(ArithmeticOperator op, int n){
+    if(op == AOP_PLUS){
+        print("+", n);
+    }
+    else if(op == AOP_MINUS){
+        print("-", n);
+    }
+    else if(op == AOP_DIV){
+        print("/", n);
+    }
+    else {
+        print("*", n);
+    }
+}
+
 void arithmeticOperator(Ast_ArithmeticOperator* t, int n){
     print("Ast_ArithmeticOperator", n);
-    printLogicalOperator(t->op,n);
+    
 
 }
 
 void moreExpansion(Ast_MoreExpansion* t, int n){
     print("Ast_MoreExpansion", n);
-    printLogicalOperator(t->op,n);
 
 }
